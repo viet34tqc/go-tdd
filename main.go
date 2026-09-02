ad8e6b76cfa5c23403953a484521adb5cf79f776
viet34tqc <viet34tqc@gmail.com>
2026-09-02 21:46:17 +0700

    Mocking

--- main.go
+++ main.go
@@ -1,36 +1,10 @@
 package main
 
-import "fmt"
-
-const (
-	spanish = "Spanish"
-	french  = "French"
-
-	englishHelloPrefix = "Hello, "
-	spanishHelloPrefix = "Hola, "
-	frenchHelloPrefix  = "Bonjour, "
+import (
+	"os"
+	"test/mocking"
 )
 
 func main() {
-	fmt.Printf("Hello")
-}
-
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-	return
+	mocking.Countdown(os.Stdout)
 }
